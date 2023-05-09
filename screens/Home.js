7c0c059e63b372ecb08f01164b9e5d4743399999
BeelzeBut <marco.buga@student.upt.ro>
2023-05-10 01:28:31 +0300

    fixed errors

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,26 @@
 
 import React from "react";
 import { View, Button, Text, ImageBackground, TouchableOpacity, StyleSheet } from 'react-native';
+import MapView from "react-native-maps";
 
 const Home = ({ navigation }) => {
   return (
-    <ImageBackground source={require('..TimisoaraMap.png')} style={{flex: 1}}>
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+<MapView
+  style={{width: '100%', height: '100%'}}
+  initialRegion={{
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  }}
+>     
+ <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text style={{fontSize: 24, fontWeight: 'bold'}}>Welcome to my app!</Text>
         <TouchableOpacity style={{marginTop: 20, backgroundColor: 'blue', padding: 10}} onPress={() => navigation.navigate('Login')}>
           <Text style={{color: 'white'}}>Press to navigate</Text>
         </TouchableOpacity>
       </View>
-    </ImageBackground>
+    </MapView>
   );
 };
   
